731738bd586d1627bf44e571ec6150f377c2a608
Austin Henriksen <austin@zeroc.com>
2021-03-30 12:38:16 -0700

    Fixed first batch of formatting and lint errors.

--- slicec-cs/src/cs_options.rs
+++ slicec-cs/src/cs_options.rs
@@ -1,15 +1,15 @@
 // Copyright (c) ZeroC, Inc. All rights reserved.
 
-use structopt::StructOpt;
 use slice::options::SliceOptions;
+use structopt::StructOpt;
 
-// Note: StructOpt automatically uses the doc-comments of fields to populate the '--help' output of slicec-cs.
+// Note: StructOpt uses the doc-comments of fields to populate the '--help' output of slicec-cs.
 //       boolean flags automatically default to false, and strings automatically default to empty.
 
-/// This struct is responsible for parsing the command line options specified to the 'slicec-cs' compiler.
-/// The option parsing capabilities are automatically generated for the struct by the `StructOpt` crate.
+/// This struct is responsible for parsing the command line options specific to 'slicec-cs'.
+/// The option parsing methods are automatically generated for the struct by the `StructOpt` crate.
 #[derive(StructOpt, Debug)]
-#[structopt(name = "slicec-cs", version = "0.1.0", rename_all = "kebab-case", about = ABOUT_MESSAGE)]
+#[structopt(name = "slicec-cs", version = "0.1.0", rename_all = "kebab-case", about = ABOUT_STRING)]
 pub struct CsOptions {
     // Import the options common to all slice compilers.
     #[structopt(flatten)]
@@ -17,4 +17,5 @@ pub struct CsOptions {
 }
 
 /// Short description of slicec-cs that is displayed in it's help dialogue.
-const ABOUT_MESSAGE: &str = "The slice compiler for C#.\nGenerates C# code from slice files for use with icerpc.";
+const ABOUT_STRING: &str = "The slice compiler for C#.
+                            Generates C# code from slice files for use with icerpc.";
