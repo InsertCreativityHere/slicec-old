3c46d24d1d6396a762f1c959b55b38f4f8c952df
Austin Henriksen <austin.r.henriksen79@gmail.com>
2021-09-13 10:09:17 -0400

    Added macros for auto-implementing grammar traits.

--- src/grammar/traits.rs
+++ src/grammar/traits.rs
@@ -50,3 +50,136 @@ pub trait Type: Element {
     fn is_fixed_size(&self) -> bool;
     fn min_wire_size(&self) -> u32;
 }
+
+macro_rules! implement_Element_for {
+    ($type:ty, $kind_string:literal) => {
+        impl Element for $type {
+            fn kind(&self) -> &'static str {
+                $kind_string
+            }
+        }
+    };
+}
+
+macro_rules! implement_Symbol_for {
+    ($type:ty) => {
+        impl Symbol for $type {
+            fn location(&self) -> &Location {
+                &self.location
+            }
+        }
+    };
+}
+
+macro_rules! implement_Named_Symbol_for {
+    ($type:ty) => {
+        impl NamedSymbol for $type {
+            fn identifier(&self) -> &String {
+                &self.identifier.value
+            }
+
+            fn raw_identifier(&self) -> &Identifier {
+                &self.identifier
+            }
+        }
+    };
+}
+
+macro_rules! implement_Scoped_Symbol_for {
+    ($type:ty) => {
+        impl ScopedSymbol for $type {
+            fn scope(&self) -> &String {
+                &self.scope.raw_scope
+            }
+
+            fn parser_scope(&self) -> &String {
+                &self.scope.raw_parser_scope
+            }
+
+            fn raw_scope(&self) -> &Scope {
+                &self.scope
+            }
+        }
+    };
+}
+
+macro_rules! implement_Attributable_for {
+    ($type:ty) => {
+        impl Attributable for $type {
+            fn attributes(&self) -> &Vec<Attribute> {
+                &self.attributes
+            }
+
+            fn has_attribute(&self, directive: &str) -> bool {
+                self.get_raw_attribute(directive).is_some()
+            }
+
+            fn get_attribute(&self, directive: &str) -> Option<&Vec<String>> {
+                self.get_raw_attribute(directive).and_then(
+                    |attribute| Some(&attribute.arguments)
+                )
+            }
+
+            fn get_raw_attribute(&self, directive: &str) -> Option<&Attribute> {
+                for attribute in &self.attributes {
+                    if attribute.prefixed_directive == directive {
+                        return Some(attribute);
+                    }
+                }
+                None
+            }
+        }
+    };
+}
+
+macro_rules! implement_Commentable_for {
+    ($type:ty) => {
+        impl Commentable for $type {
+            fn comment(&self) -> Option<&DocComment> {
+                self.comment.as_ref()
+            }
+        }
+    };
+}
+
+macro_rules! implement_Entity_for {
+    ($type:ty) => {
+        implement_Symbol_for!($type);
+        implement_Named_Symbol_for!($type);
+        implement_Scoped_Symbol_for!($type);
+        implement_Attributable_for!($type);
+        implement_Commentable_for!($type);
+
+        impl Entity for $type {}
+    };
+}
+
+macro_rules! implement_Container_for {
+    ($type:ty, $contained_type:ty, $field_name:ident) => {
+        impl Container<$contained_type> for $type {
+            fn contents(&self) -> &Vec<OwnedPtr<$contained_type>> {
+                &self.$field_name
+            }
+        }
+    };
+}
+
+macro_rules! implement_Contained_for {
+    ($type:ty, $container_type:ty) => {
+        impl Contained<$container_type> for $type {
+            fn parent(&self) -> &WeakPtr<$container_type> {
+                &self.parent
+            }
+        }
+    };
+}
+
+pub(crate) use implement_Element_for;
+pub(crate) use implement_Symbol_for;
+pub(crate) use implement_Named_Symbol_for;
+pub(crate) use implement_Scoped_Symbol_for;
+pub(crate) use implement_Attributable_for;
+pub(crate) use implement_Commentable_for;
+pub(crate) use implement_Entity_for;
+pub(crate) use implement_Container_for;
+pub(crate) use implement_Contained_for;
