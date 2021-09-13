0cbba74f99c6bcf7f5f297c5115873d2199f74a6
Austin Henriksen <austin.r.henriksen79@gmail.com>
2021-09-13 13:15:19 -0400

    Added separate module for wrappers.

---
+++ src/grammar/wrappers.rs
@@ -0,0 +1,101 @@
+// Copyright (c) ZeroC, Inc. All rights reserved.
+
+use super::slice::*;
+use super::comments::*;
+use super::traits::*;
+use super::util::*;
+
+#[derive(Debug)]
+pub enum Definition {
+    Struct(Struct),
+    Class(Class),
+    Exception(Exception),
+    Interface(Interface),
+    Enum(Enum),
+    TypeAlias(TypeAlias),
+}
+
+#[derive(Debug)]
+pub enum Types<'a> {
+    Struct(&'a Struct),
+    Class(&'a Class),
+    Interface(&'a Interface),
+    Enum(&'a Enum),
+    TypeAlias(&'a TypeAlias),
+    Sequence(&'a Sequence),
+    Dictionary(&'a Dictionary),
+    Primitive(&'a Primitive),
+}
+
+macro_rules! forward_trait_for_Definition {
+    ($type:ty) => {
+        forward_trait_for_Definition!($type, );
+    };
+    ($type:ty, $(($function:ident, $return:ty$(, $param:ident;$param_type:ty)?)),*) => {
+        impl $type for Definition {
+            $(fn $function(&self$(, $param: $param_type)?) -> $return {
+                match self {
+                    Self::Struct(x) => x.$function($($param)?),
+                    Self::Class(x) => x.$function($($param)?),
+                    Self::Exception(x) => x.$function($($param)?),
+                    Self::Interface(x) => x.$function($($param)?),
+                    Self::Enum(x) => x.$function($($param)?),
+                    Self::TypeAlias(x) => x.$function($($param)?),
+                }
+            })*
+        }
+    };
+}
+
+forward_trait_for_Definition!(Element,
+    (kind, &'static str)
+);
+forward_trait_for_Definition!(Symbol,
+    (location, &Location)
+);
+forward_trait_for_Definition!(NamedSymbol,
+    (identifier, &String),
+    (raw_identifier, &Identifier)
+);
+forward_trait_for_Definition!(ScopedSymbol,
+    (scope, &String),
+    (parser_scope, &String),
+    (raw_scope, &Scope)
+);
+forward_trait_for_Definition!(Commentable,
+    (comment, Option<&DocComment>)
+);
+forward_trait_for_Definition!(Attributable,
+    (attributes, &Vec<Attribute>),
+    (has_attribute, bool, directive;&str),
+    (get_attribute, Option<&Vec<String>>, directive;&str),
+    (get_raw_attribute, Option<&Attribute>, directive;&str)
+);
+forward_trait_for_Definition!(Entity);
+
+macro_rules! forward_trait_for_Types {
+    ($type:ty, $(($function:ident, $return:ty$(, $param:ident;$param_type:ty)?)),*) => {
+        impl<'a> $type for Types<'a> {
+            $(fn $function(&self$(, $param: $param_type)?) -> $return {
+                match self {
+                    Self::Struct(x) => x.$function($($param)?),
+                    Self::Class(x) => x.$function($($param)?),
+                    Self::Interface(x) => x.$function($($param)?),
+                    Self::Enum(x) => x.$function($($param)?),
+                    Self::TypeAlias(x) => x.$function($($param)?),
+                    Self::Sequence(x) => x.$function($($param)?),
+                    Self::Dictionary(x) => x.$function($($param)?),
+                    Self::Primitive(x) => x.$function($($param)?),
+                }
+            })*
+        }
+    };
+}
+
+forward_trait_for_Types!(Element,
+    (kind, &'static str)
+);
+forward_trait_for_Types!(Type,
+    (is_fixed_size, bool),
+    (min_wire_size, u32)
+);
