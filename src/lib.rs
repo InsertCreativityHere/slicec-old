ec99ddf6a16f39d8c41c6679617ba7d9f9902770
Austin Henriksen <austin.r.henriksen79@gmail.com>
2021-10-07 16:08:01 -0400

    Moved 'ThreadSafe' wrapper into 'slicec::Util'.

--- src/lib.rs
+++ src/lib.rs
@@ -11,12 +11,14 @@ pub mod visitor;
 
 use crate::ast::Ast;
 
+// TODO comments
 pub fn borrow_ast() -> &'static Ast {
-    global_state::AST.get().unwrap().borrow()
+    unsafe { &*global_state::AST.get().unwrap().get() }
 }
 
+// TODO comments
 pub unsafe fn borrow_mut_ast() -> &'static mut Ast {
-    global_state::AST.get().unwrap().borrow_mut()
+    &mut *global_state::AST.get().unwrap().get()
 }
 
 pub fn report_error() {
@@ -33,36 +35,17 @@ pub fn main() {
 mod global_state {
     use crate::ast::Ast;
     use crate::error::ErrorReporter;
-    use crate::util::OwnedPtr;
+    use crate::util::ThreadSafe;
     use once_cell::unsync::OnceCell;
+    use std::cell::UnsafeCell;
 
-    pub(super) static AST: ThreadSafe<Ast> = ThreadSafe(OnceCell::new());
-    pub(super) static ERROR_REPORTER: ThreadSafe<ErrorReporter> = ThreadSafe(OnceCell::new());
+    type ThreadSafeCell<T> = ThreadSafe<OnceCell<UnsafeCell<T>>>;
+
+    pub(super) static AST: ThreadSafeCell<Ast> = ThreadSafe(OnceCell::new());
+    pub(super) static ERROR_REPORTER: ThreadSafeCell<ErrorReporter> = ThreadSafe(OnceCell::new());
 
     pub(super) fn initialize() {
-        let _ = AST.set(OwnedPtr::new(Ast::new()));
-        let _ = ERROR_REPORTER.set(OwnedPtr::new(ErrorReporter::new()));
-    }
-
-    // `ThreadSafe` is a transparent wrapper for marking data as thread-safe, even if it isn't.
-    // The Rust compiler automatically infers thread-safety at compile time, but data can be
-    // explicitily marked as thread-safe by implementing the `Sync` trait on it, like here.
-    //
-    // We use this as a hack to satisfy the Rust compiler. Only thread-safe data can be stored in
-    // static variables, since it MIGHT be accessed from other threads. But since the slice
-    // compiler is single threaded, this isn't a concern.
-    //
-    // If we ever make the slice compiler multi-threaded we'd have to make the data thread-safe
-    // anyways, and then could drop this hack.
-    pub(super) struct ThreadSafe<T>(OnceCell<OwnedPtr<T>>);
-
-    unsafe impl<T> Sync for ThreadSafe<T> {}
-
-    impl<T> std::ops::Deref for ThreadSafe<T> {
-        type Target = OnceCell<OwnedPtr<T>>;
-
-        fn deref(&self) -> &Self::Target {
-            &self.0
-        }
+        let _ = AST.set(UnsafeCell::new(Ast::new()));
+        let _ = ERROR_REPORTER.set(UnsafeCell::new(ErrorReporter::new()));
     }
 }
